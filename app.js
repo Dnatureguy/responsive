b09ee4093dac665061d86b65b9e52254f25b3205
Dnatureguy <73432326+Dnatureguy@users.noreply.github.com>
2021-07-09 22:13:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function myfunction() {
+    var x = document.getElementById("gat ");
+    if (x.className === "gat ") {
+        x.className += "responsive";
+    } else {
+        x.className = "gat ";
+    }
+    
+}
+
+function myfunction() {
+    var x = document.getElementById("haf ");
+    if (x.className === "haf ") {
+        x.className += "responsive";
+    } else {
+        x.className = "haf ";
+    }
+    
+}
